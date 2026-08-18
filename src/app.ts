8d01b8f9604ecf3dbef1c58cb62e718c12812ee4
deboak <adebowaleakinode9@gmail.com>
2026-08-18 16:03:54 +0100

    add redis health

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import { redis } from "./config/redis";
 import routes from "./routes/routes"
 import { errorHandler } from "./middlewares/errorHandler";
 
@@ -16,6 +17,19 @@ app.use('/api/v1', routes)
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK', message: 'Server is healthy' });
+});
+
+app.get("/health/redis", async (_req, res, next) => {
+  try {
+    const result = await redis.ping();
+
+    res.status(200).json({
+      status: "OK",
+      redis: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use(errorHandler);
